7ec80566b830ed51a76ab7fa1911bec012e7dbaa
ToriSohva <tori@mahoushoujo.moe>
2019-09-24 14:50:49 +0300

    remove old import

--- src/app/components/question/question.component.ts
+++ src/app/components/question/question.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Apollo } from 'apollo-angular';
 import gql from 'graphql-tag';
-import { Media } from '../../models/Media';
 
 @Component({
   selector: 'app-question',
